ff7a325a9ad64e4560df7d2a8614c4ff826e7463
Gregory Pottier <130802341+gregorypottier@users.noreply.github.com>
2023-04-19 15:44:41 -0400

    Create permutation.py

---
+++ hackerrank/practice/permutation.py
@@ -0,0 +1,22 @@
+# Itertools - Permutation
+
+def permutations(string, length):
+    if length == 0:
+        return [""]
+    permutations_list = []
+    for i in range(len(string)):
+        char = string[i]
+        remaining_string = string[:i] + string[i+1:]
+        sub_permutations = permutations(remaining_string, length-1)
+        print(sub_permutations)
+        for permutation in sub_permutations:
+            permutations_list.append(char + permutation)
+    return permutations_list
+
+string, k = input().split(" ")
+permutationLength = int(k)
+
+plist = permutations(string, permutationLength)
+plist.sort()
+for p in plist:
+    print(p)
