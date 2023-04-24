9768ea9aae5adc29db9200ba148af73a0d046781
Gregory Pottier <130802341+gregorypottier@users.noreply.github.com>
2023-04-24 03:36:27 -0400

    Create maxit.py

---
+++ hackerrank/practice/itertools/maxit.py
@@ -0,0 +1,14 @@
+# Itertools - maximize it
+from itertools import product
+
+k, mod = map(int, input().split())
+
+ll = []
+
+for i in range(k):
+    sl = list(map(lambda x: int(x)**2, input().split(" ")[1:])) # first number indicates size
+    ll.append(sl) # append squared values to ll
+# cartesian product of all the sublists and computing the remainder of their sum when divided by m
+allProducts = list(product(*ll))
+result = list(sum(x) % mod for x in allProducts)
+print(max(result))
