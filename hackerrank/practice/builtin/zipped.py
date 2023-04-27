6ef9bea624ab36927957db8f536802cd8553d475
Gregory Pottier <130802341+gregorypottier@users.noreply.github.com>
2023-04-26 20:19:07 -0400

    Update zipped.py

--- hackerrank/practice/builtin/zipped.py
+++ hackerrank/practice/builtin/zipped.py
@@ -4,7 +4,7 @@ numstudents, numsubjects = map(int, input().split())
 
 gradeList = [list(map(float, input().split()))for _ in range(numsubjects)]
     
-studentGrades = zip(*gradeList) # unpack a list of lists
+studentGrades = zip(*gradeList) # unpack a list of lists, then zip elements
 
 for sg in studentGrades:
     print(sum(sg)/numsubjects)
